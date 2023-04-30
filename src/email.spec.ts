a3a7ec9d87fe3a2ea9a64038f5029696cff32705
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-04-30 08:05:23 -0300

    tests: react email parsing to html working

--- src/email.spec.ts
+++ src/email.spec.ts
@@ -1,9 +1,19 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, assert } from 'vitest'
 import { sendEmail } from './email'
+import { BaseTemplate } from './templates/base_template'
+import { render } from '@react-email/render'
 
 describe('Email', () => {
-  it('Sends an Email', async () => {
+
+  it.skip('Sends an Email', async () => {
     const response = await sendEmail()
     expect(response).toBeTruthy()
   })
+
+  it('Parses a React Component to a Html String', () => {
+    const html = render(BaseTemplate())
+    expect(html).toBeTruthy()
+    assert.isString(html)
+  })
+
 })
---
+++ src/templates/base_template.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Html } from '@react-email/html'
+
+export function BaseTemplate() {
+    return (
+      <Html>
+        <h1>From React</h1>
+      </Html>
+    )
+}
